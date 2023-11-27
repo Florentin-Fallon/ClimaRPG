a6562702d05adc50814b93a9e50ccaf5a52cd680
Cacahuète <24205659+CacahueteSansSel@users.noreply.github.com>
2023-11-27 10:55:03 +0100

    added nice rendering perks

--- src/rendering.py
+++ src/rendering.py
@@ -5,14 +5,35 @@ import pytmx
 class Raylib:
 
     def __init__(self):
+        set_config_flags(ConfigFlags.FLAG_WINDOW_RESIZABLE)
         init_window(640, 480, 'Clima RPG')
+        self.width = 640
+        self.height = 480
+        self.render_texture: RenderTexture = load_render_texture(self.width, self.height)
 
     def run(self, render, update):
         while not window_should_close():
             begin_drawing()
+
+            clear_background(BLACK)
+            begin_texture_mode(self.render_texture)
             clear_background(BLACK)
             self.render()
             render()
+            end_texture_mode()
+
+            renderWidth = get_render_height() / self.height * self.width
+
+            draw_texture_pro(self.render_texture.texture,
+                             Rectangle(0, 0, self.width, -self.height),
+                             Rectangle(get_render_width() / 2 - renderWidth / 2,
+                                       0,
+                                       renderWidth,
+                                       get_screen_height()),
+                             Vector2(0, 0),
+                             0,
+                             WHITE)
+
             end_drawing()
 
             self.update()
@@ -21,6 +42,7 @@ class Raylib:
         self.dispose()
 
     def dispose(self):
+        unload_render_texture(self.render_texture)
         close_window()
 
     def render(self):
